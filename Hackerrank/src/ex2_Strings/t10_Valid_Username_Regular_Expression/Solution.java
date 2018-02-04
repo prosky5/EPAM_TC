97af2718cea92eca215eddddf5eb9e3b570c0e2c
prosky5 <prosky25@gmail.com>
2018-02-04 19:48:32 +0300

    Hackerrank. Finished ex2_Strings (Complete t9-t11 - "RegEx").

--- Hackerrank/src/ex2_Strings/t10_Valid_Username_Regular_Expression/Solution.java
+++ Hackerrank/src/ex2_Strings/t10_Valid_Username_Regular_Expression/Solution.java
@@ -1,4 +1,24 @@
 package ex2_Strings.t10_Valid_Username_Regular_Expression;
 
+import java.util.Scanner;
+
+class UsernameValidator {
+    public static final String regularExpression = "^[a-zA-Z][\\w]{7,29}$";
+}
+
 public class Solution {
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+        }
+    }
 }
--- Hackerrank/src/ex2_Strings/t11_Tag_Content_Extractor/Solution.java
+++ Hackerrank/src/ex2_Strings/t11_Tag_Content_Extractor/Solution.java
@@ -1,4 +1,35 @@
 package ex2_Strings.t11_Tag_Content_Extractor;
 
-public class Solution {
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution{
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases>0){
+            String line = in.nextLine();
+            //Write your code here
+
+            Pattern p = Pattern.compile("<(.+)>([^<>]+)</\\1>", Pattern.DOTALL); //<(\w+)>(.*?)</\\1>
+            Matcher m = p.matcher(line);
+
+            boolean checkFind = false;
+
+            while (m.find()) {
+                System.out.println(m.group(2));
+                checkFind = true;
+            }
+
+            if (!checkFind) {
+                System.out.println("None");
+            }
+
+            testCases--;
+        }
+    }
 }
